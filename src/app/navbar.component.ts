9fea9d7d7c594e660587ca8ffc30ff8fbfb84ad3
JeffCarroll58 <jeff@carrollengr.com>
2022-11-25 10:37:51 -0600

    1 file modified

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "navbar",
   template: `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-1">
       <a class="navbar-brand" href="#">Navbar</a>
     </nav>
   `
